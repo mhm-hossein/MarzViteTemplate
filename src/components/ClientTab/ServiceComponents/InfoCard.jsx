2bdafc974b0199c9627356954e0f40ad68bfa236
Mhm <155957634+mhm-hossein@users.noreply.github.com>
2025-04-25 19:42:10 +0330

    InfoCard.jsx

--- src/components/ClientTab/ServiceComponents/InfoCard.jsx
+++ src/components/ClientTab/ServiceComponents/InfoCard.jsx
@@ -1,7 +1,7 @@
 import { Card, Col } from "react-bootstrap";
 
 const InfoCard = ({ title, value, ltr }) => (
-  <Col xs="12" md="3">
+  <Col xs="12" md="4">
     <Card>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
